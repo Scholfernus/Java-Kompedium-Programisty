926648952c58f7470e768f53620d222a0dd97535
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-07-11 21:26:10 +0200

    TwoDArray Helion

---
+++ src/main/java/JavaBasic/TwoDArray.java
@@ -0,0 +1,19 @@
+package JavaBasic;
+
+public class TwoDArray {
+    public static void main(String[] args) {
+        int[][] two = new int[4][5];
+        int i, j, k = 0;
+        for (i = 0; i < 4; i++)
+            for (j = 0; j < 5; j++) {
+                two[i][j] = k;
+                k++;
+            }
+        for (i = 0; i < 4; i++) {
+            for (j = 0; j < 5; j++)
+                System.out.print(two[i][j] + " ");
+            System.out.println();
+        }
+    }
+}
+
