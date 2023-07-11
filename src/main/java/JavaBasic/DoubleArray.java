76122da427a2a6a73a0da71820db29cbb9d265f5
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-07-11 23:01:56 +0200

    Tablica wielowymiarowa  - deklaracja zmniennych

---
+++ src/main/java/JavaBasic/DoubleArray.java
@@ -0,0 +1,18 @@
+package JavaBasic;
+
+public class DoubleArray {
+    public static void main(String[] args) {
+        double[][] n = {
+                {0*0,1*0,2*0,3*0},
+                {0*1,1*1,2*1,3*1},
+                {0*2,1*2,2*2,3*2},
+                {0*3,1*3,2*3,3*3}
+        };
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                System.out.print(n[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
