6aeb35e7dbe7607d3615f46daaa398a7704ca2a1
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-07-12 23:46:48 +0200

    If Else

---
+++ src/main/java/JavaBasic/InstrukcjeElseIfPage105.java
@@ -0,0 +1,18 @@
+package JavaBasic;
+
+public class InstrukcjeElseIfPage105 {
+    public static void main(String[] args) {
+        int month = 4;
+        String season;
+        if (month == 12 || month == 1 || month == 2) {
+            season = "winter";
+        } else if (month == 3 || month == 4 || month == 5) {
+            season = "spring";
+        } else if (month == 6 || month == 7 || month == 8) {
+            season = "summer";
+        } else if (month == 9 || month == 10 || month == 11) {
+            season = "autumn";
+        } else season = "Błędny miesiąc";
+        System.out.println("Miesiąc kwiecień należy do pory roku " + season + ".");
+    }
+}
